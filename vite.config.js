ca0709e770b4f429f402e044a894026b51e6b307
Emircan Mert <emircanmertt@gmail.com>
2025-05-23 10:39:43 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/emircanmert/', // ← GitHub repo adınla birebir aynı olmalı
   plugins: [react()],
   resolve: {
     alias: {

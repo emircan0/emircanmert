fe5db36a9f3d719a4357214ee55f1620e57be075
Emircan Mert <emircanmertt@gmail.com>
2025-06-25 22:58:18 +0300

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <p>© 2025 Emircan Mert. All rights reserved.</p>
       <div className={styles.socials}>
         <a href="https://github.com/emircan0" target="_blank" rel="noopener noreferrer">GitHub</a> | 
-        <a href="https://linkedin.com/in/emircantmert" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/emircanmert/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       </div>
     </footer>
   );
